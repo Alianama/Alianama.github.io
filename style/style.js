ee0446717530fa10908674602e1be3805afcc099
Alipurnama <alipurnama69@gmail.com>
2023-05-29 22:58:45 +0700

    penambahan javascript

--- style/style.js
+++ style/style.js
@@ -7,6 +7,11 @@ let mountains_front = document.getElementById("mountains_front");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  stars.style.left = value + "px";
+  stars.style.left = value * 0.25 + "px";
+  moon.style.top = value * 1 + "px";
+  mountains_behind.style.top = value * 0.5 + "px";
+  mountains_front.style.top = value * 0 + "px";
+  text.style.marginLeft = value * 4 + "px";
+  text.style.marginTop = value * 1.5 + "px";
+  button.style.marginTop = value * 1.5 + "px";
 });
-
